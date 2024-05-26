4c21bb1657ed3474462e982869d0bd602b99fee7
ommaniar <109684810+ommaniar@users.noreply.github.com>
2024-05-26 16:28:00 +0530

    Update 29.py

--- 29.py
+++ 29.py
@@ -10,10 +10,9 @@ def remove_outliers(df,col):
     Q1 = df[col].quantile(0.25)
     Q3 = df[col].quantile(0.75)
     IQR = Q3 - Q1
-    min = Q1 -1.5 * IQR
-    max = Q3  + 1.5 * IQR
-    df[(df[col]>= max) & (df[col] <= min)] = df[col].mean
-    return df
+    lower_bound = Q1 -1.5 * IQR
+    upper_bound = Q3  + 1.5 * IQR
+    return df[(df[col]<= upper_bound) & (df[col] >= lower_bound)] 
 df = remove_outliers(df,"fare")
 
 print(df["fare"])
